cc2f787fead63950d7e0c818767712a596ce4a0a
karankraina <karanraina1996@gmail.com>
2018-05-05 19:16:59 +0530

    Updated

--- javascript.js
+++ javascript.js
@@ -1,15 +1,25 @@
 var db = firebase.database();
+var c = 6;
 function myfunction(){
-	window.alert('Update in Progress');
-	db.ref().child('Check').set(parseInt(document.getElementById('checkq').value));
-	db.ref().child('Head').set(document.getElementById('headq').value);
-	db.ref().child('Message').set(document.getElementById('messageq').value);
-	db.ref().child('MessageFS').set(parseInt(document.getElementById('msq').value));
-	db.ref().child('Title').set(document.getElementById('titq').value);
-	db.ref().child('TitleFS').set(parseInt(document.getElementById('tsq').value));
+	if (document.getElementById('checkq').value != ""){
+		db.ref().child('Check').set(parseInt(document.getElementById('checkq').value));
+	}
+	if (document.getElementById('headq').value!=""){
+		db.ref().child('Head').set(document.getElementById('headq').value);
+	}
+	if (document.getElementById('messageq').value!=""){
+		db.ref().child('Message').set(document.getElementById('messageq').value);
+	}
+	if (document.getElementById('msq').value!=""){
+		db.ref().child('MessageFS').set(parseInt(document.getElementById('msq').value));
+	}
+	if (document.getElementById('titq').value!=""){
+		db.ref().child('Title').set(document.getElementById('titq').value);
+	}
+	if (document.getElementById('tsq').value!=""){
+		db.ref().child('TitleFS').set(parseInt(document.getElementById('tsq').value));
+	}
 	
-	
-
 	window.alert('Data Updated');
 
 
